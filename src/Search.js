aa526b4254307f329d2965f0e9973840670b76f7
amccaskey9 <114969372+amccaskey9@users.noreply.github.com>
2023-08-11 18:47:59 -0500

    Create Search
    Imported axios and created search engine for weather app

---
+++ src/Search.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  function searchCity(event) {
+    event.preventDefault();
+    let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={searchCity}>
+      <input
+        type="search"
+        placeholder="Type a city..."
+        onChange={updateCity}
+      ></input>
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {Math.round(weather.humidity)}%</li>
+          <li>Wind Speed: {Math.round(weather.wind)}km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
