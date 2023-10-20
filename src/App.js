efd8e1bffa70af35358e05668bebe88bdcaf1cb7
amccaskey9 <114969372+amccaskey9@users.noreply.github.com>
2023-10-20 17:39:33 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ function App() {
   return (
     <div className="App">
       <Search />
+      <footer>
+        <p>
+          This project was coded by Aiyana McCaskey and is open-sourced on{" "}
+          <a
+            href="https://github.com/amccaskey9/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://marvelous-licorice-80709c.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
